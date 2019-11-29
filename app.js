796bca509f59677c37ac794baedc0a15f5a2b406
Mark Cheli <mpcheli7@gmail.com>
2019-11-29 10:46:58 -0500

    Added express proxy

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 var express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 var app = express();
 
-app.get('/', function(req, res) {
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+app.get('/api/', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
 });
 
-app.post('/', function(req, res) {
+app.post('/api/', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
 });
 
+app.get('/api/contact', function(req, res) {
+  console.log(req.apiGateway.event);
+  res.send({
+    "Output": "Hello World!"
+  });
+});
+
+app.post('/api/contact', function(req, res) {
+  console.log(req.apiGateway.event);
+  res.send({
+    "Output": "Hello World!"
+  });
+});
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
